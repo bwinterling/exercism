51574e065e4086382ceeb2522424dd30a45e679f
Brian Winterling <bwinterling@yahoo.com>
2013-12-04 14:57:43 -0700

    submitted bob.js, etl.rb

---
+++ javascript/bob/bob.js
@@ -0,0 +1,15 @@
+var Bob = function() {
+  // this.hey = function(input) {
+  //   if (input.replace(/\s/g,"") == "") {
+  //     return "Fine. Be that way!";
+  //   } else if (input.toUpperCase() == input && input.match(/[a-zA-Z]/) !== null ) {
+  //     return "Woah, chill out!";
+  //   } else if (input.substr(input.length-1) === "?") {
+  //     return "Sure.";
+  //   } else {
+  //     return "Whatever.";
+  //   }
+  // };
+}
+
+// exports.Bob = Bob;
