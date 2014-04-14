83618eca13b21247df0b80414578c86859d88115
Brian Winterling <bwinterling@itriagehealth.com>
2014-04-14 11:28:03 -0600

    refactored grains.js

--- javascript/grains/grains.js
+++ javascript/grains/grains.js
@@ -1,10 +1,11 @@
-function Grains(){
-  this.square = function(num){
-    return Math.pow(2,(num - 1));
-  };
-  this.total = function(){
-    return this.square(65);
-  };
+function Grains(){};
+
+Grains.prototype.square = function(n) {
+  return Math.pow(2, (n-1))
+};
+
+Grains.prototype.total = function() {
+  return this.square(65)
 };
 
 module.exports = Grains;
