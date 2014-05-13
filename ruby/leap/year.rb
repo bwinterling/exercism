01315309b2fb74eef3481a087024eae57ca1d035
Brian Winterling <bwinterling@itriagehealth.com>
2014-05-13 12:10:20 -0600

    completed space age and gigasecond

--- ruby/leap/year.rb
+++ ruby/leap/year.rb
@@ -6,22 +6,11 @@ class Year
   end
 
   def leap?
-    (vanilla_leap_year? && !century?) || exceptional_century?
+    return true if divisible_by?(400)
+    divisible_by?(4) && !divisible_by?(100)
   end
 
 private
-  def vanilla_leap_year?
-    year_divisible_by(4)
-  end
-
-  def century?
-    year_divisible_by(100)
-  end
-
-  def exceptional_century?
-    year_divisible_by(400)
-  end
-
   def divisible_by?(num)
     year % num == 0
   end
